0add315326716811cd5c0d8f3213bbdd5c5a1ea7
田向阳 <254711559@qq.com>
2018-12-19 18:17:05 +0800

    自定义单元格

--- listview/lib/ListView/TableView.dart
+++ listview/lib/ListView/TableView.dart
@@ -16,8 +16,8 @@ class TableView extends StatelessWidget {
     );
   }
 
-  List<TableViewCell> _children() {
-    return _models.map((model) => new TableViewCell(model)).toList();
+  List<CunstomTableViewCell> _children() {
+    return _models.map((model) => new CunstomTableViewCell(model)).toList();
   }
 }
 
@@ -33,5 +33,62 @@ class TableViewCell extends StatelessWidget {
             child: new Text(_model.name[0])),
         title: new Text("姓名：" + _model.name),
         subtitle: new Text("年龄：" + _model.age));
+  }
+}
+
+class CunstomTableViewCell extends StatelessWidget {
+
+  final ListModel _model;
+  CunstomTableViewCell(this._model);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.only(top: 10,left: 10,right: 20),
+      child: new Row(
+        children: [
+          new Container(
+            child: new CircleAvatar(
+                child: new Text(_model.name[1])
+            ),
+            padding: const EdgeInsets.only(right: 10),
+          ),
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 0.0),
+                  child: new Text(
+                    "姓名：" + _model.name,
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                new Text(
+                  "年龄：" + _model.age,
+                  style: new TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          new Row(
+            children: <Widget>[
+              new Icon(
+                Icons.star,
+                color: Colors.red[500],
+              ),
+              new Text('41'),
+            ],
+          )
+
+        ],
+      ),
+    );
+    return titleSection;
   }
 }
